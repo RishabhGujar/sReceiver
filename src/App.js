f3ab676906ecd246a2140f1ae0398315598ccc28
RishabhGujar <rishabhgurjar101@gmail.com>
2021-11-25 11:31:18 +0530

    "hls source"

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ export default function App() {
     responsive: true,
     fluid: true,
     poster:
-      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerMeltdowns.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/8/8f/Sintel_poster.jpg",
 
     sources: [
       {
-        src: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4",
-        type: "video/mp4",
+        src: "https://multiplatform-f.akamaihd.net/i/multi/april11/sintel/sintel-hd_,512x288_450_b,640x360_700_b,768x432_1000_b,1024x576_1400_m,.mp4.csmil/master.m3u8",
+        type: "application/x-mpegURL",
       },
     ],
   };
